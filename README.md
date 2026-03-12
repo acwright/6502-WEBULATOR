192844d70d8a170a9a897947612eb871b3e8c106
Aaron Wright <aaron.c.wright@icloud.com>
2026-03-11 21:40:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 6502 Webulator
+
+Web base emulator app for the [A.C. Wright 6502 project](https://github.com/acwright/6502).
---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="bg-white text-black">
+    <h1 class="text-4xl">6502</h1>
+  </header>
+  <main class="h-full">
+    Test
+    <span class="font-chivo">01 23 45 FF</span>
+    <span class="text-gray-500">$012345</span>
+  </main>
+</template>
+
+<script setup lang="ts"></script>
